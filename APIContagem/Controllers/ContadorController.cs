8031fb4e93585d2f0ec558709bacdc56b2efd242
Renato Groffe <renato.groffe@yahoo.com.br>
2019-12-02 22:11:28 -0300

    Update ContadorController.cs

--- APIContagem/Controllers/ContadorController.cs
+++ APIContagem/Controllers/ContadorController.cs
@@ -23,7 +23,7 @@ namespace APIContagem.Controllers
                 {
                     _CONTADOR.ValorAtual,
                     Environment.MachineName,
-                    Local = "Canal .NET",
+                    Local = "Canal .NET DevWeek 2019",
                     Sistema = Environment.OSVersion.VersionString,
                     Variavel = configuration["TesteAmbiente"],
                     TargetFramework = Assembly
